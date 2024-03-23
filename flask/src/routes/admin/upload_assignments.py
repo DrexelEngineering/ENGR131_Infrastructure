efb7c31673db7fe253e6e43b92845aa044fe3bea
jagar2 <jca92@drexel.edu>
2024-03-22 22:08:18 -0400

    upload assignments docstring

--- flask/src/routes/admin/upload_assignments.py
+++ flask/src/routes/admin/upload_assignments.py
@@ -1,13 +1,6 @@
 import os
 import sys
 from ...util.string_util import is_string_number_combo, extract_string_number
-
-# Get the directory of your current script
-current_dir = os.path.dirname(__file__)
-# Go one folder back
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
-
 from file_translators.assignment_reader import assignment_JSON
 from flask_login import login_required
 from util.sql_util import upsert_json, commit_sql
@@ -15,89 +8,100 @@ from flask import Blueprint, request
 import time
 from login.login import admin_login
 
+# Adjusting the system path to include the parent directory for relative imports
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
 upload_assignment = Blueprint('upload_assignment', __name__)
 
 @upload_assignment.route('/upload_assignment', methods=['POST'])
 @login_required
 def upload_students_():
+    """
+    Uploads an assignment and its associated questions to the database.
     
-    # Check for assignmentName in the form data
+    Validates the incoming request for required fields ('password', 'assignment_name', 'assignment_id', and 'file'),
+    parses the uploaded JSON file for assignment details, and inserts or updates the assignment and question records
+    in the database.
+    
+    Requires user authentication.
+    
+    Returns:
+        str: A message indicating the result of the operation. Returns HTTP status code 400 for client errors
+            and 500 for server-side SQL execution errors.
+    """
+    
+    # Validation for required form data 'password'
     if 'password' not in request.form:
         return 'password is required', 400
     password = request.form['password']
     
+    # Establish a database connection as an admin
     conn = admin_login(password)
     cursor = conn.cursor()
     
-    # Check if the request has the file part
+    # Validation for the file part in the request
     if 'file' not in request.files:
         return 'No file part in the request', 400
     file = request.files['file']
     
-    # Check for assignmentName in the form data
+    # Validation for 'assignment_name' in form data
     if 'assignment_name' not in request.form:
         return 'assignment_name is required', 400
     assignment_name = request.form['assignment_name']
     
-    # Check for assignmentID in the form data
+    # Validation and parsing for 'assignment_id' in form data
     if 'assignment_id' not in request.form:
         return 'assignment_name is required', 400
     else:
-        assignment_id = request.form['assignment_id'] 
+        assignment_id = request.form['assignment_id']
         if is_string_number_combo(assignment_id):
             assignment_type, assignment_num = extract_string_number(assignment_id)
         else:
-            return 'assignment_id must be a string in the format AssignmentType_AssignmentNumber', 400
+            return 'assignment_id must be a string-number combo in the format AssignmentType_AssignmentNumber', 400
 
-    # If the user does not select a file, the browser submits an empty file without a filename
+    # Check for empty filename submission
     if file.filename == '':
         return 'No selected file', 400
 
     if file:
-        
-        # Save the file to a temporary file
+        # Process the uploaded file
         temp_path = './temp_uploaded_file.JSON'
         file.save(temp_path)
-
-        # need to parse the extracted json file
         json_extracted = assignment_JSON(temp_path)
+        os.remove(temp_path)  # Clean up by removing the temporary file
         
-        os.remove(temp_path)  # Remove the temporary file
+        # Preparing data for insertion into the 'assignments' table
+        assignment_data = [{"assignment_name": assignment_name,
+                            "assignment_id": assignment_id,
+                            "assignment_type": assignment_type,
+                            "assignment_num": assignment_num}]
         
-        # Upsert into Assignment_table
-        dict = [{"assignment_name": assignment_name,
-                "assignment_id": assignment_id,
-                "assignment_type": assignment_type,
-                "assignment_num": assignment_num}]
-        
-        sql_call = upsert_json(dict, 'assignments', 'assignment_id')        
-        out = commit_sql(cursor, sql_call)
-        if not out:
-            conn.rollback()  # Rollback in case of failure
+        # Insert or update assignment record
+        sql_call = upsert_json(assignment_data, 'assignments', 'assignment_id')        
+        if not commit_sql(cursor, sql_call):
+            conn.rollback()  # Rollback and close connection on failure
             conn.close()
             return "Error occurred during SQL execution", 500
         
-        dict = []
-        
-        # loops around all the entries in the json file and adds them to the dict for the question record
+        question_data = []
+        # Parsing each question from the JSON and preparing data for insertion
         for data in json_extracted:
-            
-            dict.append({"question_id": data.get('question_id', ''),
-                        "assignment_id": assignment_id,
-                        "question_name": data.get('questions_name', ''),
-                        "max_score": data.get('max_score', ''),
-                        "visible": data.get('visibility', '')})
+            question_data.append({"question_id": data.get('question_id', ''),
+                                "assignment_id": assignment_id,
+                                "question_name": data.get('questions_name', ''),
+                                "max_score": data.get('max_score', ''),
+                                "visible": data.get('visibility', '')})
 
-        # Upsert into Question_table
-        sql_call = upsert_json(dict, 'questions', ["assignment_id", "question_id"])   
-        out = commit_sql(cursor, sql_call)
-        if not out:
-            conn.rollback()  # Rollback in case of failure
+        # Insert or update question records
+        sql_call = upsert_json(question_data, 'questions', ["assignment_id", "question_id"])
+        if not commit_sql(cursor, sql_call):
+            conn.rollback()  # Rollback and close connection on failure
             conn.close()
             return "Error occurred during SQL execution", 500
 
+        # Commit transactions and close the connection
         conn.commit()
-        # need to add the sql call here
         conn.close()
         return "success"
-
